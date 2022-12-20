916a31d4b5b4ef7016e231630678b9523d63b4f9
augusta-dev <109660213+augusta-dev@users.noreply.github.com>
2022-12-20 21:14:41 +0300

    Updated

--- slideshow.js
+++ slideshow.js
@@ -1,15 +1,15 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let itemss = document.querySelectorAll('.carousel .carousel-item')
 
 function rede() {
     if (window.innerWidth <= 1100 ) {
-        items.forEach((el) => {
+        itemss.forEach((el) => {
             // number of slides per carousel-item
             const minPerSlide = 2
             let next = el.nextElementSibling
             for (var i=1; i<minPerSlide; i++) {
                 if (!next) {
                     // wrap carousel by using first child
-                    next = items[0]
+                    next = itemss[0]
                 }
                 let cloneChild = next.cloneNode(true)
                 el.appendChild(cloneChild.children[0])
@@ -17,14 +17,14 @@ function rede() {
             }
         })
     } else {
-        items.forEach((el) => {
+        itemss.forEach((el) => {
             // number of slides per carousel-item
             const minPerSlide = 3
             let next = el.nextElementSibling
             for (var i=1; i<minPerSlide; i++) {
                 if (!next) {
                     // wrap carousel by using first child
-                    next = items[0]
+                    next = itemss[0]
                 }
                 let cloneChild = next.cloneNode(true)
                 el.appendChild(cloneChild.children[0])
